a1ef495f7f8e9f8683bdfa00e0bef8eeef7fa3c9
tallytutor <Dan@daniels-air.attlocal.net>
2015-12-20 02:41:11 -0500

    Stylistic edits and mobile optimization
    changed fonts, redid music submenu, added content, optimized mobile,
    debugged for android chrome

--- wordpress/wp-content/themes/awfulkid/custom.js
+++ wordpress/wp-content/themes/awfulkid/custom.js
@@ -1,16 +1,12 @@
 jQuery(document).ready(function() {
-	jQuery(".children").addClass("nav");
-	jQuery(".children a").css("padding-left", "30px");
-	jQuery(".children a").css("margin-top", "0px");
-	jQuery(".children a").css("margin-bottom", "0px");
-	jQuery(".children a").css("margin-left", "0px");
+
 
 	jQuery('h1.awkLogo').mouseenter(function(){
 		jQuery('h1.awkLogo').addClass('animated pulse');
 	});
 
 	jQuery('h1.awkLogo').mouseleave(function(){
-		jQuery('h1.awkLogo').delay(950).removeClass('animated pulse');
+		jQuery('h1.awkLogo').removeClass('animated pulse');
 	});
 
 	jQuery('a').click(function (e) {
@@ -25,4 +21,6 @@ jQuery(document).ready(function() {
 	         window.location = goTo;
 	    },250);       
 	});
+
+	
 });
--- wordpress/wp-content/themes/awfulkid/header.php
+++ wordpress/wp-content/themes/awfulkid/header.php
@@ -1,9 +1,11 @@
+<?php wp_head(); ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1, user-scalable=no">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="Awful Kid is an assortment of artists who create with honesty, ambition, and perserverance.">
     <meta name="author" content="Dan Maiman & Ryan Kachikis">
@@ -11,7 +13,7 @@
 
     <title>Awful Kid</title>
 
-    <?php wp_head(); ?>
+
 
     <!-- Bootstrap core CSS -->
     <!--<link href="dist/css/bootstrap.min.css" rel="stylesheet"> Kept for reference-->
@@ -42,7 +44,7 @@
 
     <div class="container">
 
-      <a href="<?php echo get_settings('home'); ?>"><h1 class="awkLogo">AWFUL <br>KID</h1></a>
+      <a href="<?php echo get_settings('home'); ?>"><h1 class="awkLogo">AWFUL<br>KID</h1></a>
 
 
 <div class="nav-cont">
@@ -53,25 +55,62 @@
   </nav>
 </div>
 
+<nav class="navbar navbar-default navbar-fixed-top hidden-md hidden-lg">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="<?php echo get_settings('home'); ?>">Awfulkid</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <?php wp_list_pages('title_li=&depth=1'); ?>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
 
 
 <div class="awk_content animated bounceInRight">
   <?php
+    $children = wp_list_pages('title_li=&child_of='.$post->ID.'&echo=0');
+      if ($children) { ?>
+        
+
+        <ul class="nav nav-pills nav-stacked nav-music">
+          <?php echo $children; ?>
+        </ul>
+  <?php } ?>
+
+  <?php
     while(have_posts()) : the_post(); ?>
 
       <p><?php the_content(''); ?></p>
+      
 
-  <?php endwhile; wp_reset_query(); ?>
+    <?php endwhile; wp_reset_query(); ?>
 </div>
 
+   </div><!-- /.container -->
 
 
-<!-- Nav bar collapse for mobile
-<div id="navbar" class="collapse navbar-collapse">
-  <ul class="nav navbar-nav">
-    <li class="active"><a href="#">Home</a></li>
-    <li><a href="#about">About</a></li>
-    <li><a href="#contact">Contact</a></li>
-  </ul>
-</div>--><!--/.nav-collapse -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <!-- <script src="dist/js/bootstrap.min.js"></script>-->
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <!--<script src="assets/js/ie10-viewport-bug-workaround.js"></script> -->
+  </body>
+</html>
+
+
+
 
